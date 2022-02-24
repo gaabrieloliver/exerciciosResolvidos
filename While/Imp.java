77c299117ddabd46a9f5ee8147ab05ab539bbc5c
Gabriel Oliveira <74208625+gaabrieloliver@users.noreply.github.com>
2022-02-24 08:44:47 -0300

    Create Imp.java

---
+++ While/Imp.java
@@ -0,0 +1,30 @@
+package javaproject;
+
+import java.util.Scanner;
+/*IMPRIMIR NA TELA 
+public class Imp {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        boolean scape = (true);
+
+        while (scape) {
+            int i = in.nextInt();
+            if (i > 0 && i < 100) {
+                scape = false;
+            }
+
+            i++;
+            while (i < 98) {
+                if (i % 2 == 1) {
+                    System.out.print(i + ", ");
+                }
+                    i++;
+
+            }
+            System.out.print(99);
+        }
+
+    }
+}
