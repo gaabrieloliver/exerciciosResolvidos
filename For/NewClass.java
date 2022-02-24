dec4b639548b90cad33f99705269e78a8fa5a128
Gabriel Oliveira <74208625+gaabrieloliver@users.noreply.github.com>
2022-02-24 09:08:14 -0300

    Add files via upload

---
+++ For/NewClass.java
@@ -0,0 +1,17 @@
+
+package javaproject;
+
+import java.util.Scanner;
+public class NewClass {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+             
+    for (int i=1; i <= 99; i+=2){
+        System.out.print(i+",");
+       
+    }     
+        System.out.println(100);
+        
+        in.close();
+}
+   
