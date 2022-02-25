7936625e912f7acb73daa56b3dc5c752cb67f8c3
Gabriel Oliveira <74208625+gaabrieloliver@users.noreply.github.com>
2022-02-24 21:54:23 -0300

    Add files via upload

---
+++ if_else/exe5pdf2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/*Dois garotos querem dividirr a melância que compraram no mercado,
+porém querem dividir de forma que esteja em par
+ */
+public class exe5pdf2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Peso da melância: ");
+        double w = in.nextDouble();
+
+        /*Esse é o cálculo pra saber se o número é par, pelo resto da divisão*/
+
+        if(w % 2 == 0)
+            System.out.println("Pode ser cortada com peso par");
+            else
+            System.out.println("Não pode ser cortada com peso par");
+
+        }
+
+
+
+
+    }
+
