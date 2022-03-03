c8ade7cb76e951b76070db116cc4a82c4ebcd434
Gabriel Oliveira <74208625+gaabrieloliver@users.noreply.github.com>
2022-03-03 11:18:43 -0300

    Update MaiorMedia.java

--- Do_While/MaiorMedia.java
+++ Do_While/MaiorMedia.java
@@ -2,25 +2,27 @@
 // maior número e a média desses números.
 
 import java.util.Scanner;
-public class MaiorMedia {
+public class Ex3_MaiorMedia {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         int num;
 
-        int cont = 0;
+        int cont = 1;
         int maior = 0;
+        int soma=0;
 
         do {
         System.out.print("Numero: ");
         num = in.nextInt();
         if(num > maior)
             maior = num;
+        soma=soma+num;
         cont++;
 
         }
-        while(cont < 5);
+        while(cont <= 5);
         System.out.println("Maior é: "+maior);
-
+        System.out.println("Média é: "+soma/5);
     }
 }
