446d0a31a5858f75244a4ce15dfb5b9ff6079fe2
Gabriel Oliveira <74208625+gaabrieloliver@users.noreply.github.com>
2022-03-05 06:26:39 -0300

    Create tabuada.java

---
+++ For/tabuada.java
@@ -0,0 +1,33 @@
+public class Ex5_Tabuada {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("TABUADA");
+
+        int tab = 0;
+
+        System.out.print("Qual a tabuada?\nDigite: ");
+        tab = in.nextInt();
+
+        for (int i = 0; i <= 10; i++){
+            System.out.println(tab+" x "+i+" = "+tab*i);
+
+        }
+
+    }
+}
+
+/*Resultado
+TABUADA
+Qual a tabuada?
+Digite: 5
+5 x 0 = 0
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50*/
