381e9e807ebf36d09f7f1bbb32bdf4e3255bfb05
Gabriel Oliveira <74208625+gaabrieloliver@users.noreply.github.com>
2022-03-05 06:27:47 -0300

    Update tabuada.java

--- For/tabuada.java
+++ For/tabuada.java
@@ -1,3 +1,6 @@
+/*Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10. 
+O usuário deve informar de qual numero ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:*/
+
 public class Ex5_Tabuada {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
